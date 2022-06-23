e727d910adfa5604f15b087d25fb0f1c130ec362
Miguel Palop <mpalop@moneytrans.eu>
2022-06-23 16:48:49 +0200

    added initDB to initial data for database

---
+++ wallaclone/initDB.js
@@ -0,0 +1,87 @@
+'use strict';
+
+require('dotenv').config();
+const fsPromise = require('fs/promises');
+const readline = require('readline');
+
+const dbConnection = require('./lib/connectMongoose');
+
+const Adverts = require('./models/Adverts');
+const User = require('./models/User');
+
+dbConnection.once('open', () => {
+  main().catch(err => console.log('Hubo un error', err));
+});
+
+async function main() {
+  const respQuestion = await question(
+    'Estas seguro de que quieres borrar la base de datos? (si/no)',
+  );
+  if (!respQuestion) {
+    process.exit(0);
+  }
+
+  // inicializar articulos
+  await initAdverts();
+
+  // inicializar usuarios
+  await initUsers();
+
+  // desconectar la base de datos
+  dbConnection.close();
+}
+
+async function initUsers() {
+  // borrar los usuarios existentes
+  const deleted = await User.deleteMany();
+  console.log(`Eliminados ${deleted.deletedCount} usuarios.`);
+
+  // crear usuarios
+  const usuarios = await User.insertMany([
+    {
+        email: 'user1@wallaclone.es',
+        name: 'user1',
+        password: await User.hashPassword('1234'),
+        role: 'admin',
+    },
+    {
+        email: 'user2@wallaclone.es',
+        name: 'user2',
+        password: await User.hashPassword('1234'),
+        role: 'user',
+    },
+  ]);
+  console.log(`Creados ${usuarios.length} usuarios.`);
+}
+
+async function initAdverts() {
+  // borrar todos los articulos que haya en la colección
+  const deleted = await Adverts.deleteMany();
+  console.log(`Eliminados ${deleted.deletedCount} articulos.`);
+
+  const data = await fsPromise.readFile('adverts.json', 'utf-8');
+  const articuloData = JSON.parse(data);
+
+  // crear articulos iniciales
+  const articulos = await Articulo.insertMany(articuloData);
+  console.log(`Creados ${articulos.length} articulos.`);
+}
+
+function question(texto) {
+  return new Promise((resolve, reject) => {
+    // conectar readline a la consola
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    // hacemos pregunta
+    rl.question(texto, respuesta => {
+      rl.close();
+      if (respuesta.toLowerCase() === 'si') {
+        resolve(true);
+        return;
+      }
+      resolve(false);
+    });
+  });
+}
