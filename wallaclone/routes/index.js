1e9a8187b71d71b1aefde11b9190a03bdf6490c7
Jose Antonio Cascales <jose.cascalesbo@gmail.com>
2022-07-01 14:13:04 +0200

    poner querys por tags y otros filtros

--- wallaclone/routes/index.js
+++ wallaclone/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+// Filtros por query string por tags, precios, nombre
+
+router.get('/tags/:tags(work|lifestyle|mobile|motor|IT)/price/:price?/name/:name?', (req, res, next) => {
+  const tags = req.params.tags;
+  //const color = req.params.color;
+  //const price = req.params.price;
+  //const name = req.params.name;
+
+  res.send(`ok tags ${tags}`) //color, price, name} )
+})
+
 module.exports = router;
+////price/:price?/name/:name?
