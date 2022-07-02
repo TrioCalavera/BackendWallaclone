655ec8848640d40f9c7b8e9813c73aa5aa091c16
Basilio <basilio.uvinas@gmail.com>
2022-07-02 18:42:03 +0200

    Feat: Propuesta inicializar la DB con npm start initDB. Comentado varias lineas que nos daban problemas. Revisar.

--- README.md
+++ README.md
@@ -6,10 +6,17 @@ To start the applicaion use:
    install dependencies:
    npm install
 ```
+
+```sh
+   Inicializar DB
+   npm start initDB
+```
+
 ```sh
    run the app PROD:
    npm start
 ```
+
 ```sh
    run the app DEV:
    DEBUG=wallaclone:* npm start
--- wallaclone/initDB.js
+++ wallaclone/initDB.js
@@ -1,21 +1,23 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const fsPromise = require('fs/promises');
-const readline = require('readline');
+require("dotenv").config();
+const fsPromise = require("fs/promises");
+const readline = require("readline");
 
-const dbConnection = require('./lib/connectMongoose');
+const dbConnection = require("./lib/connectMongoose");
 
-const Adverts = require('./models/Advert');
-const User = require('./models/User');
+const Adverts = require("./models/Advert");
+const User = require("./models/User");
 
-dbConnection.once('open', () => {
-  main().catch(err => console.log('Hubo un error', err));
+const advertData = require("./adverts.json");
+
+dbConnection.once("open", () => {
+  main().catch((err) => console.log("Hubo un error", err));
 });
 
 async function main() {
   const respQuestion = await question(
-    'Estas seguro de que quieres borrar la base de datos? (si/no)',
+    "Estas seguro de que quieres borrar la base de datos? (si/no)"
   );
   if (!respQuestion) {
     process.exit(0);
@@ -39,16 +41,16 @@ async function initUsers() {
   // crear usuarios
   const usuarios = await User.insertMany([
     {
-        email: 'user1@wallaclone.es',
-        name: 'user1',
-        password: await User.hashPassword('1234'),
-        role: 'admin',
+      email: "user1@wallaclone.es",
+      name: "user1",
+      password: await User.hashPassword("1234"),
+      role: "admin",
     },
     {
-        email: 'user2@wallaclone.es',
-        name: 'user2',
-        password: await User.hashPassword('1234'),
-        role: 'user',
+      email: "user2@wallaclone.es",
+      name: "user2",
+      password: await User.hashPassword("1234"),
+      role: "user",
     },
   ]);
   console.log(`Creados ${usuarios.length} usuarios.`);
@@ -59,11 +61,11 @@ async function initAdverts() {
   const deleted = await Adverts.deleteMany();
   console.log(`Eliminados ${deleted.deletedCount} articulos.`);
 
-  const data = await fsPromise.readFile('adverts.json', 'utf-8');
-  const articuloData = JSON.parse(data);
+  // const data = await fsPromise.readFile(advertData, "utf-8");
+  // const articuloData = JSON.parse(data);
 
   // crear articulos iniciales
-  const articulos = await Adverts.insertMany(articuloData);
+  const articulos = await Adverts.insertMany(advertData);
   console.log(`Creados ${articulos.length} articulos.`);
 }
 
@@ -75,9 +77,9 @@ function question(texto) {
       output: process.stdout,
     });
     // hacemos pregunta
-    rl.question(texto, respuesta => {
+    rl.question(texto, (respuesta) => {
       rl.close();
-      if (respuesta.toLowerCase() === 'si') {
+      if (respuesta.toLowerCase() === "si") {
         resolve(true);
         return;
       }
