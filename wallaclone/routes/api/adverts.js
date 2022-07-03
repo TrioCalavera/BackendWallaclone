9cb9aa8a8cc4a0d030b16075f2791ab15164af03
Basilio <basilio.uvinas@gmail.com>
2022-07-03 12:21:52 +0200

    Feat: Devolver un Json con todos los anuncios. Crear filtros para paginación, ordenar, etc..., modificar schema para devolver filtros. QUitadas las mayúsculas del schema del modelo de anuncios. REvisar si routes/api/adverts es necesario

--- wallaclone/routes/api/adverts.js
+++ wallaclone/routes/api/adverts.js
@@ -1,19 +1,18 @@
-'use strict'
+"use strict";
 
-const express = require('express');
-const Advert = require('../../models/Advert');
+const express = require("express");
+const Advert = require("../../models/Advert");
 const router = express.Router();
-
 
 //GET /api/adverts
 
-router.get("/", async(req, res, next) => {
-    try {
-        const adverts = await Advert.find();
-        res.json({ result: adverts})
-    } catch (error) {
-        next(err);
-    }
+router.get("/advert", async (req, res, next) => {
+  try {
+    const adverts = await Advert.find();
+    res.json({ result: adverts });
+  } catch (error) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- wallaclone/routes/index.js
+++ wallaclone/routes/index.js
@@ -1,22 +1,67 @@
-var express = require('express');
+var express = require("express");
+const Advert = require("../models/Advert");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+// Traer todos los anuncios
+router.get("/adverts", async (req, res, next) => {
+  try {
+    const tags = req.query.tags;
+    const price = req.query.price;
+    const name = req.query.name;
+    const sale = req.query.sale;
+
+    // Pagination
+    const skip = req.query.limit;
+
+    // Limits the amount
+    const limit = req.query.limit;
+
+    //Fields to show
+    const select = req.query.select;
+
+    //Field to sort
+    const sort = req.query.sort;
+
+    // Obj filtros
+    const filtros = {};
+
+    if (name) {
+      filtros.name = name;
+    }
+
+    if (price) {
+      filtros.price = price;
+    }
+
+    if (sale) {
+      filtros.sale = sale;
+    }
+
+    if (tags) {
+      filtros.tags = tags;
+    }
+
+    // const adverts = await Advert.find();
+    const adverts = await Advert.lista(filtros, skip, limit, select, sort);
+    res.json({ result: adverts });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Filtros por query string por tags, precios, nombre
+// router.get("/adverts", (req, res, next) => {
+//   const tags = req.query.tags;
+//   const price = req.query.price;
+//   const name = req.query.name;
+//   const sale = req.query.sale;
+//   console.log(req.query);
 
-router.get('/filterquerystring', (req, res, next) => {
-  const tags = req.query.tags;
-  //const color = req.params.color;
-  const price = req.query.price;
-  const name = req.query.name;
-  console.log(req.query);
-
-  res.send(`ok tags ${tags} name ${name} price ${price}`); //price, name} )
-});
-
+//   // res.send(`ok tags ${tags} name ${name} price ${price}`); //price, name} )
+// });
 module.exports = router;
